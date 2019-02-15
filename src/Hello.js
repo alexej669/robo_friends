dc601f8685a9a682a2fcd7c86df671238fc373bc
alexej669 <alexej669@gmail.com>
2019-02-15 13:03:01 +0200

    Hello world with func based component

--- src/Hello.js
+++ src/Hello.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hello.css';
 
 
-class Hello extends Component {
-render() 
-{
-	return(
-	<div className = 'f1 tc'	>
+const Hello = (props) => { 
+
+return(
+	<div className = 'f1 tc'>
 		<h1> Hello world </h1>
-		<p> {this.props.greeting}</p>
+		<p> { props.greeting }</p>
 	</div>	
-		);
-}
+	);
+
 }
 
-export default Hello
+export default Hello;
